d63ef33e3f57a08bbaba72521c86b576d8f73957
teego8 <47618093+teego8@users.noreply.github.com>
2019-02-14 14:30:01 +0800

    Update receive.php

--- receive.php
+++ receive.php
@@ -9,155 +9,124 @@
   	$sender_txt = $json_obj->events[0]->message->text; //取得訊息內容
   	$sender_replyToken = $json_obj->events[0]->replyToken; //取得訊息的replyToken
   	$msg_json = '{
-		"type": "bubble",
-		"hero": {
-		  "type": "image",
-		  "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
-		  "size": "full",
-		  "aspectRatio": "20:13",
-		  "aspectMode": "cover",
-		  "action": {
-			"type": "uri",
-			"uri": "http://linecorp.com/"
-		  }
-		},
-		"body": {
-		  "type": "box",
-		  "layout": "vertical",
-		  "contents": [
-			{
-			  "type": "text",
-			  "text": "Brown Cafe",
-			  "weight": "bold",
-			  "size": "xl"
-			},
-			{
-			  "type": "box",
-			  "layout": "baseline",
-			  "margin": "md",
-			  "contents": [
-				{
-				  "type": "icon",
-				  "size": "sm",
-				  "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-				},
-				{
-				  "type": "icon",
-				  "size": "sm",
-				  "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-				},
-				{
-				  "type": "icon",
-				  "size": "sm",
-				  "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-				},
-				{
-				  "type": "icon",
-				  "size": "sm",
-				  "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-				},
-				{
-				  "type": "icon",
-				  "size": "sm",
-				  "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png"
-				},
-				{
-				  "type": "text",
-				  "text": "4.0",
-				  "size": "sm",
-				  "color": "#999999",
-				  "margin": "md",
-				  "flex": 0
-				}
-			  ]
-			},
-			{
-			  "type": "box",
-			  "layout": "vertical",
-			  "margin": "lg",
-			  "spacing": "sm",
-			  "contents": [
-				{
-				  "type": "box",
-				  "layout": "baseline",
-				  "spacing": "sm",
-				  "contents": [
-					{
-					  "type": "text",
-					  "text": "Place",
-					  "color": "#aaaaaa",
-					  "size": "sm",
-					  "flex": 1
-					},
-					{
-					  "type": "text",
-					  "text": "Miraina Tower, 4-1-6 Shinjuku, Tokyo",
-					  "wrap": true,
-					  "color": "#666666",
-					  "size": "sm",
-					  "flex": 5
-					}
-				  ]
-				},
-				{
-				  "type": "box",
-				  "layout": "baseline",
-				  "spacing": "sm",
-				  "contents": [
-					{
-					  "type": "text",
-					  "text": "Time",
-					  "color": "#aaaaaa",
-					  "size": "sm",
-					  "flex": 1
-					},
-					{
-					  "type": "text",
-					  "text": "10:00 - 23:00",
-					  "wrap": true,
-					  "color": "#666666",
-					  "size": "sm",
-					  "flex": 5
-					}
-				  ]
-				}
-			  ]
-			}
-		  ]
-		},
-		"footer": {
-		  "type": "box",
-		  "layout": "vertical",
-		  "spacing": "sm",
-		  "contents": [
-			{
-			  "type": "button",
-			  "style": "link",
-			  "height": "sm",
-			  "action": {
-				"type": "uri",
-				"label": "CALL",
-				"uri": "https://linecorp.com"
-			  }
-			},
-			{
-			  "type": "button",
-			  "style": "link",
-			  "height": "sm",
-			  "action": {
-				"type": "uri",
-				"label": "WEBSITE",
-				"uri": "https://linecorp.com"
-			  }
-			},
-			{
-			  "type": "spacer",
-			  "size": "sm"
-			}
-		  ],
-		  "flex": 0
-		}
-	  }';
+  "type": "flex",
+  "altText": "Flex Message",
+  "contents": {
+    "type": "bubble",
+    "hero": {
+      "type": "image",
+      "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_2_restaurant.png",
+      "size": "full",
+      "aspectRatio": "20:13",
+      "aspectMode": "cover",
+      "action": {
+        "type": "uri",
+        "label": "Action",
+        "uri": "https://linecorp.com"
+      }
+    },
+    "body": {
+      "type": "box",
+      "layout": "vertical",
+      "spacing": "md",
+      "action": {
+        "type": "uri",
+        "label": "Action",
+        "uri": "https://linecorp.com"
+      },
+      "contents": [
+        {
+          "type": "text",
+          "text": "Brown's Burger",
+          "size": "xl",
+          "weight": "bold"
+        },
+        {
+          "type": "box",
+          "layout": "vertical",
+          "spacing": "sm",
+          "contents": [
+            {
+              "type": "box",
+              "layout": "baseline",
+              "contents": [
+                {
+                  "type": "icon",
+                  "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/restaurant_regular_32.png"
+                },
+                {
+                  "type": "text",
+                  "text": "$10.5",
+                  "flex": 0,
+                  "margin": "sm",
+                  "weight": "bold"
+                },
+                {
+                  "type": "text",
+                  "text": "400kcl",
+                  "size": "sm",
+                  "align": "end",
+                  "color": "#AAAAAA"
+                }
+              ]
+            },
+            {
+              "type": "box",
+              "layout": "baseline",
+              "contents": [
+                {
+                  "type": "icon",
+                  "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/restaurant_large_32.png"
+                },
+                {
+                  "type": "text",
+                  "text": "$15.5",
+                  "flex": 0,
+                  "margin": "sm",
+                  "weight": "bold"
+                },
+                {
+                  "type": "text",
+                  "text": "550kcl",
+                  "size": "sm",
+                  "align": "end",
+                  "color": "#AAAAAA"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "type": "text",
+          "text": "Sauce, Onions, Pickles, Lettuce & Cheese",
+          "size": "xxs",
+          "color": "#AAAAAA",
+          "wrap": true
+        }
+      ]
+    },
+    "footer": {
+      "type": "box",
+      "layout": "vertical",
+      "contents": [
+        {
+          "type": "spacer",
+          "size": "xxl"
+        },
+        {
+          "type": "button",
+          "action": {
+            "type": "uri",
+            "label": "Add to Cart",
+            "uri": "https://linecorp.com"
+          },
+          "color": "#905C44",
+          "style": "primary"
+        }
+      ]
+    }
+  }
+}';
   	$response = array (
 		"replyToken" => $sender_replyToken,
 		"messages" => array (
