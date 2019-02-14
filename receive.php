469412cca9858ca91b6e7e198056b1e9faf09fad
teego8 <47618093+teego8@users.noreply.github.com>
2019-02-14 14:05:37 +0800

    Update receive.php

--- receive.php
+++ receive.php
@@ -1,52 +1,53 @@
 <?php
-	$json_str = file_get_contents('php://input'); //接收request的body
-	$json_obj = json_decode($json_str); //轉成json格式
+  	$json_str = file_get_contents('php://input'); //接收request的body
+  	$json_obj = json_decode($json_str); //轉成json格式
   
-	$myfile = fopen("log.txt", "w+") or die("Unable to open file!"); //設定一個log.txt來印訊息
-	fwrite($myfile, "\xEF\xBB\xBF".$json_str); //在字串前面加上\xEF\xBB\xBF轉成utf8格式
+  	$myfile = fopen("log.txt", "w+") or die("Unable to open file!"); //設定一個log.txt來印訊息
+  	//fwrite($myfile, "\xEF\xBB\xBF".$json_str); //在字串前面加上\xEF\xBB\xBF轉成utf8格式
   
-	$sender_userid = $json_obj->events[0]->source->userId; //取得訊息發送者的id
-	$sender_txt = $json_obj->events[0]->message->text; //取得訊息內容
-	$sender_replyToken = $json_obj->events[0]->replyToken; //取得訊息的replyToken
-  
-	$response = array (
+  	$sender_userid = $json_obj->events[0]->source->userId; //取得訊息發送者的id
+  	$sender_txt = $json_obj->events[0]->message->text; //取得訊息內容
+  	$sender_replyToken = $json_obj->events[0]->replyToken; //取得訊息的replyToken
+  	$msg_json = '{
+		"type": "template",
+		"altText": "this is a carousel template",
+		"template": {
+		  "type": "carousel",
+		  "actions": [],
+		  "columns": [
+			{
+			  "title": "標題",
+			  "text": "文字",
+			  "actions": [
+				{
+				  "type": "message",
+				  "label": "動作 1",
+				  "text": "動作 1"
+				}
+			  ]
+			},
+			{
+			  "title": "標題",
+			  "text": "文字",
+			  "actions": [
+				{
+				  "type": "uri",
+				  "label": "動作 1",
+				  "uri": "https://www.google.com.tw/"
+				}
+			  ]
+			}
+		  ]
+		}
+	  }';
+  	$response = array (
 		"replyToken" => $sender_replyToken,
 		"messages" => array (
-			array (
-				"type" => "template",
-				"altText" => "this is a buttons template",
-				"template" => array (
-					"type" => "buttons",
-					"thumbnailImageUrl" => "https://www.w3schools.com/css/paris.jpg",
-					"title" => "Menu",
-					"text" => "Please select",
-					"actions" => array (
-						array (
-							"type" => "postback",
-							"label" => "Buy",
-							"data" => "action=buy&itemid=123"
-						),
-						array (
-							"type" => "message",
-							"label" => "Return",
-							"text" => "This is text"
-						),
-						array (
-							"type" => "datetimepicker",
-							"label" => "Select date",
-							"data" => "storeId=12345",
-							"mode" => "datetime",
-							"initial" => "2017-12-25t00:00",
-							"max" => "2018-01-24t23:59",
-							"min" => "2017-12-25t00:00"
-						)
-					)
-				)
-	  		)
+			json_decode($msg_json)
 		)
   	);
 			
-  	//fwrite($myfile, "\xEF\xBB\xBF".json_encode($response)); //在字串前面加上\xEF\xBB\xBF轉成utf8格式
+  	fwrite($myfile, "\xEF\xBB\xBF".json_encode($response)); //在字串前面加上\xEF\xBB\xBF轉成utf8格式
   	$header[] = "Content-Type: application/json";
   	$header[] = "Authorization: Bearer 6qhDIhQ++5j1febihav54eoLSlhGIbDJyE+yy95rx0tWd4X7pLUwfOsTlavVXXFPBzBMYZyuI21X9wLrsu+aAqM+iB1UE+UaTyKItGWzqouOAaEfwuSnRwDxeVx+Iz4fpwcNzdzAZ2iU/mBreIEk0gdB04t89/1O/w1cDnyilFU=";
   	$ch = curl_init("https://api.line.me/v2/bot/message/reply");
