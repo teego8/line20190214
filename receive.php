7576fab7a3c5f09aa04d175cdc545d8e26dbd652
teego8 <47618093+teego8@users.noreply.github.com>
2019-02-14 10:59:04 +0800

    Update receive.php

--- receive.php
+++ receive.php
@@ -1,6 +1,6 @@
 <?php
- $json-str = file_get_contents('php://input'); //接收request的body
- $json_obj = json_decode($json-str); //轉成json格
+ $json_str = file_get_contents('php://input'); //接收request的body
+ $json_obj = json_decode($json_str); //轉成json格
  $myfile = fopen("log.txt", "w+") or die("Unable to open file !"); //設定一個log.txt來印訊息.
  fwrite($myfile, "\xEF\xBB\xBF".$json_str); //在字串前面加上\xEF\xBB\xBF轉成utf8格式
  ?>
